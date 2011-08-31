b10ab5ef393d40e38b585166a04256d7f00b9c05
Steven Pothoven <steven@pothoven.net>
2011-08-31 13:48:45 -0400

    attempt to fix quotes

--- app/models/blood_test.rb
+++ app/models/blood_test.rb
@@ -7,10 +7,10 @@ class BloodTest < ActiveRecord::Base
 
   default_scope order(:test_date, :test_type_id)
 
-  scope :breakfast, joins(:test_type).where('test_types.name = "breakfast"')
-  scope :lunch, joins(:test_type).where('test_types.name = "lunch"')
-  scope :dinner, joins(:test_type).where('test_types.name = "dinner"')
-  scope :bedtime, joins(:test_type).where('test_types.name = "bedtime"')
+  scope :breakfast, joins(:test_type).where("test_types.name = 'breakfast'")
+  scope :lunch, joins(:test_type).where("test_types.name = 'lunch'")
+  scope :dinner, joins(:test_type).where("test_types.name = 'dinner'")
+  scope :bedtime, joins(:test_type).where("test_types.name = 'bedtime'")
   scope :test_type, lambda { |*args| { :conditions => "test_type_id = #{TestType[args.first].id}" } }
 
   scope :recent, lambda { |*args| {:conditions => ["test_date > ?", (args.first || 1.week.ago)] } }
