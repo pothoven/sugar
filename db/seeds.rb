4e15ed43a9e9109af46fcc2ee1d83b84b99cfb2f
Steven Pothoven <steven@pothoven.net>
2011-09-02 10:53:19 -0400

    Added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -117,6 +117,15 @@ BloodTest.create([{ :test_date => '2011-07-19', :test_type_id => 1, :result => 1
                   { :test_date => '2011-08-29', :test_type_id => 3, :result => 127},
                   { :test_date => '2011-08-29', :test_type_id => 4, :result => 118},
                   { :test_date => '2011-08-30', :test_type_id => 1, :result => 100},
-                  { :test_date => '2011-08-30', :test_type_id => 3, :result => 117}])
+                  { :test_date => '2011-08-30', :test_type_id => 3, :result => 117},
+				  { :test_date => '2011-08-30', :test_type_id => 4, :result => 83},
+				  { :test_date => '2011-08-31', :test_type_id => 1, :result => 12},
+				  { :test_date => '2011-08-31', :test_type_id => 3, :result => 87},
+				  { :test_date => '2011-08-31', :test_type_id => 4, :result => 99},
+				  { :test_date => '2011-09-01', :test_type_id => 1, :result => 98},
+				  { :test_date => '2011-09-01', :test_type_id => 3, :result => 15},
+				  { :test_date => '2011-09-01', :test_type_id => 4, :result => 86},
+				  { :test_date => '2011-09-02', :test_type_id => 1, :result => 11}
+])
 
 
